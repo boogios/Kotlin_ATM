fd3881f9df3ff84278c4e4035bce3ad80a90f68c
Chanhook <cksgur97@gmail.com>
2022-11-06 18:45:48 +0900

    테스트

--- app/src/main/java/com/example/atm/MainActivity.kt
+++ app/src/main/java/com/example/atm/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-비
+
 //        pr 준비
     }
 }
