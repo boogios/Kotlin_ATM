2b554f2c9554677382ec608650faf115b98a5708
Chanhook <cksgur97@gmail.com>
2022-11-07 01:22:06 +0900

    추가 replaceFragment()
    추가 뷰 바인딩 활성화 및 아이템 클릭될 때마다 화면 전환

--- app/src/main/java/com/example/atm/MainActivity.kt
+++ app/src/main/java/com/example/atm/MainActivity.kt
@@ -2,12 +2,40 @@ package com.example.atm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.atm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
 
-//        pr 준비
+        binding.bottomNavigationView2.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(Home())
+                R.id.chat -> replaceFragment(Chat())
+                R.id.settings -> replaceFragment(Settings())
+                else -> {
+
+                }
+
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+
     }
 }
