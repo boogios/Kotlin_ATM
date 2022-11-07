25b8a2ce42e0a062bd3ac173f524cc4163c83263
tkddn0518 <tkddn0518@naver.com>
2022-11-07 22:11:17 +0900

    LoginActivity & SignupActivity 연결

--- app/src/main/java/com/example/atm/LoginActivity.kt
+++ app/src/main/java/com/example/atm/LoginActivity.kt
@@ -1,11 +1,23 @@
 package com.example.atm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.atm.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignup.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/atm/SignupActivity.kt
+++ app/src/main/java/com/example/atm/SignupActivity.kt
@@ -1,11 +1,24 @@
 package com.example.atm
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.atm.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBackToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
