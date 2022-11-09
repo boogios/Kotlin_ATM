11f7a4b66aa034b362a2f1b5f5416561365ed639
tkddn0518 <tkddn0518@naver.com>
2022-11-09 15:16:12 +0900

    로그인 기능 구현

--- app/src/main/java/com/example/atm/LoginActivity.kt
+++ app/src/main/java/com/example/atm/LoginActivity.kt
@@ -3,26 +3,64 @@ package com.example.atm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.atm.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+
+    // FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // FirebaseAuth
+        auth = FirebaseAuth.getInstance()
+
         binding.btnSignup.setOnClickListener {
-            val intent = Intent(this, SignupActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, SignupActivity::class.java))
         }
 
         binding.btnLogin.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            val email = binding.loginID.text.toString()
+            val password = binding.loginPassword.text.toString()
+            logIn(email, password)
         }
-
     }
+
+//    public override fun onStart() {
+//        super.onStart()
+//        goToMainPage(auth?.currentUser) // 로그아웃 안했을 때 자동 로그인 기능
+//    }
+
+    private fun logIn(id: String, password: String) {
+        if (id.isNotEmpty() && password.isNotEmpty()) {
+            auth?.signInWithEmailAndPassword(id, password)
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(baseContext, "로그인 성공", Toast.LENGTH_LONG).show()
+                        goToMainPage(auth?.currentUser)
+                    } else {
+                        Toast.makeText(baseContext, "로그인 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+    }
+
+    // 파이어베이스에 유저 정보 전달 후 메인 페이지로 이동하는 함수
+    private fun goToMainPage(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
 }
