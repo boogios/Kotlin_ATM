85c2d2f7af8179ebb3fe266c8a482941fc10a67e
Chanhook <cksgur97@gmail.com>
2022-11-10 21:26:48 +0900

    추가 Join data class
    추가 recycler view의 하나의 리스트마다 적용될 data class

---
+++ app/src/main/java/com/example/atm/Join.kt
@@ -0,0 +1,10 @@
+package com.example.atm
+
+data class Join(
+    var profileImage: Int,
+    var nickname: String,
+    var origin: String,
+    var destination: String,
+    var numberOfMember: Int
+)
+
