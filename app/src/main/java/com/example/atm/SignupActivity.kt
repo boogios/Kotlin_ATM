765cabb8850cb9d167326e31fc11bcd8a28356da
tkddn0518 <tkddn0518@naver.com>
2022-11-10 22:53:58 +0900

    비밀번호 체크 기능 추가 (비밀번호, 비밀번호 확인 일치 체크 && 비밀번호 6자리 이상)

--- app/src/main/java/com/example/atm/SignupActivity.kt
+++ app/src/main/java/com/example/atm/SignupActivity.kt
@@ -3,8 +3,11 @@ package com.example.atm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
+import androidx.core.view.isInvisible
 import com.example.atm.databinding.ActivitySignupBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -25,12 +28,15 @@ class SignupActivity : AppCompatActivity() {
         // FirebaseAuth
         auth = FirebaseAuth.getInstance()
 
+        // Password Check
+        checkCorrectPassword()
+
         // Create Account Button
         binding.btnCreateAccount.setOnClickListener {
             val email = binding.signupEmail.text.toString()
             val password = binding.signupPassword.text.toString()
-
-            createAccount(email, password)
+            val passwordCheck = binding.signupPasswordCheck.text.toString()
+            createAccount(email, password, passwordCheck)
         }
 
         // 뒤로가기 버튼
@@ -42,17 +48,53 @@ class SignupActivity : AppCompatActivity() {
     }
 
     // 계정 생성 함수
-    private fun createAccount(id: String, password: String) {
+    private fun createAccount(id: String, password: String, passwordCheck: String) {
         if (id.isNotEmpty() && password.isNotEmpty()) {
-            auth?.createUserWithEmailAndPassword(id, password)
-                    ?.addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            Toast.makeText(this, "계정 생성 완료", Toast.LENGTH_LONG).show()
-                            finish()
-                        } else {
-                            Toast.makeText(this, "계정 생성 실패", Toast.LENGTH_LONG).show()
-                        }
+            if (password.length < 6) {
+                Toast.makeText(this, "비밀번호는 6글자 이상 이여야 합니다", Toast.LENGTH_LONG).show()
+            } else {
+                if (password != passwordCheck) {
+                    Toast.makeText(this, "비밀번호가 일치하지 않습니다", Toast.LENGTH_LONG).show()
+                } else {
+                    auth?.createUserWithEmailAndPassword(id, password)
+                        ?.addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                Toast.makeText(this, "계정 생성 완료", Toast.LENGTH_LONG).show()
+                                finish()
+                            } else {
+                                Toast.makeText(this, "계정 생성 실패", Toast.LENGTH_LONG).show()
+                            }
                     }
+                }
+            }
         }
     }
+
+    // 비밀번호 체크 함수
+    private fun checkCorrectPassword() {
+        binding.signupPasswordCheck.addTextChangedListener(object : TextWatcher {
+
+            // 문자 입력 전
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+
+            // 문자 변경 후
+            override fun afterTextChanged(p0: Editable?) {
+
+                val password = binding.signupPassword.text.toString()
+                val passwordCheck = binding.signupPasswordCheck.text.toString()
+                val txtCheck = binding.txtPasswordCheck
+
+                if (password != passwordCheck) {
+                    txtCheck.isInvisible = false
+                    txtCheck.setText("Password is not correct.. :(")
+                } else {
+                    txtCheck.setText("Password is correct!! :)")
+                }
+            }
+
+            // 문자 변경할 때마다 호출
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+        })
+    }
+
 }
