497eb0d9b548bdfb5943c4b1e2ebfeb4ed9f44ba
Chanhook <cksgur97@gmail.com>
2022-11-14 18:41:02 +0900

    추가 카카오맵 API 키워드 검색 기능
    리사이클러뷰 연결
    리스트 아이템 클릭 시 해당 위치로 이동
    키워드 검색 함수
    마커 추가 함수

--- app/src/main/java/com/example/atm/MapActivity.kt
+++ app/src/main/java/com/example/atm/MapActivity.kt
@@ -3,7 +3,12 @@ package com.example.atm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.atm.databinding.ActivityMapBinding
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -11,7 +16,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MapActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMapBinding
+    private lateinit var binding: ActivityMapBinding
+    private val listItems = arrayListOf<ListLayout>() // 리사이클러 뷰 아이템
+    private val listAdapter = ListAdapter(listItems) // 리사이클러 뷰 어댑터
+    private var pageNumber = 1 // 검색 페이지 번호
+    private var keyword = "" // 검색 키워드
 
     companion object {
         const val BASE_URL = "https://dapi.kakao.com/"
@@ -20,35 +29,113 @@ class MapActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        searchKeyword("은행")
+        // 리사이클러 뷰
+        binding.rvList.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.rvList.adapter = listAdapter
+
+        // 리스트 아이템 클릭 시 해당 위치로 이동
+        listAdapter.setItemClickListener(object : ListAdapter.OnItemClickListener {
+            override fun onClick(v: View, position: Int) {
+                Log.d("LocalSearch", "listItem: ${listItems[position]}")
+                val mapPoint =
+                    MapPoint.mapPointWithGeoCoord(listItems[position].y, listItems[position].x)
+                binding.mapView.setMapCenterPointAndZoomLevel(mapPoint, 1, true)
+            }
+        })
+
+        //검색 버튼
+        binding.btnSearch.setOnClickListener {
+            keyword = binding.etSearchField.text.toString()
+            pageNumber = 1
+            searchKeyword(keyword, pageNumber)
+        }
+
+        //이전 페이지 버튼
+        binding.btnPrevPage.setOnClickListener {
+            pageNumber--
+            binding.tvPageNumber.text = pageNumber.toString()
+            searchKeyword(keyword, pageNumber)
+        }
+
+        //다음 페이지 버튼
+        binding.btnNextPage.setOnClickListener {
+            pageNumber++
+            binding.tvPageNumber.text = pageNumber.toString()
+            searchKeyword(keyword, pageNumber)
+        }
     }
 
+
+    //==method==//
     // 키워드 검색 함수
-    private fun searchKeyword(keyword: String) {
+    private fun searchKeyword(keyword: String, page: Int) {
         val retrofit = Retrofit.Builder()   // Retrofit 구성
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         val api = retrofit.create(KakaoAPI::class.java)   // 통신 인터페이스를 객체로 생성
-        val call = api.getSearchKeyword(API_KEY, keyword)   // 검색 조건 입력
+        val call = api.getSearchKeyword(API_KEY, keyword, page)   // 검색 조건 입력
 
         // API 서버에 요청
-        call.enqueue(object: Callback<ResultSearchKeyword> {
+        call.enqueue(object : Callback<ResultSearchKeyword> {
             override fun onResponse(
                 call: Call<ResultSearchKeyword>,
                 response: Response<ResultSearchKeyword>
             ) {
                 // 통신 성공 (검색 결과는 response.body()에 담겨있음)
-                Log.d("Test", "Raw: ${response.raw()}")
-                Log.d("Test", "Body: ${response.body()}")
+                addItemsAndMarkers(response.body())
             }
 
             override fun onFailure(call: Call<ResultSearchKeyword>, t: Throwable) {
                 // 통신 실패
-                Log.w("MainActivity", "통신 실패: ${t.message}")
+                Log.w("LocalSearch", "통신 실패: ${t.message}")
             }
         })
+    }
+
+    private fun addItemsAndMarkers(searchResult: ResultSearchKeyword?) {
+        if (!searchResult?.documents.isNullOrEmpty()) {
+            // 검색 결과 있음
+            listItems.clear() // 리스트 초기화
+            binding.mapView.removeAllPOIItems() // 지도의 마커 모두 제거
+            for (document in searchResult!!.documents) {
+                // 결과를 리사이클러 뷰에 추가
+                val item = ListLayout(
+                    document.place_name,
+                    document.road_address_name,
+                    document.address_name,
+                    document.x.toDouble(),
+                    document.y.toDouble()
+                )
+                listItems.add(item)
+
+                // 지도에 마커 추가
+                val point = MapPOIItem()
+                point.apply {
+                    itemName = document.place_name
+                    mapPoint = MapPoint.mapPointWithGeoCoord(
+                        document.y.toDouble(),
+                        document.x.toDouble()
+                    )
+                    markerType = MapPOIItem.MarkerType.BluePin
+                    selectedMarkerType = MapPOIItem.MarkerType.RedPin
+                }
+                binding.mapView.addPOIItem(point)
+            }
+            listAdapter.notifyDataSetChanged()
+
+            binding.btnNextPage.isEnabled = !searchResult.meta.is_end // 페이지가 더 있을 경우 다음 버튼 활성화
+            binding.btnPrevPage.isEnabled = pageNumber != 1 // 1페이지가 아닐 경우 이전 버튼 활성화
+
+        } else {
+            // 검색 결과 없음
+            Toast.makeText(this, "검색 결과가 없습니다", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
