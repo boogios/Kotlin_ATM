2dcf978884ae17e71b0c92a6eaa8fd44e1b3f8a7
tkddn0518 <tkddn0518@naver.com>
2022-11-16 10:27:44 +0900

    Chat 관련 UI

---
+++ app/src/main/java/com/example/atm/ChatLayout.kt
@@ -0,0 +1,11 @@
+package com.example.atm
+
+import android.media.Image
+import android.widget.ImageView
+
+class ChatLayout(
+    val nickname: String,
+    val contents: String
+    // val time: String
+    // val userImage: ImageView
+)
