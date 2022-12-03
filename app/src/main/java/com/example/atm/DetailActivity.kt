cfb895a8b47ce6b1ad9a0a6d493033c3f176ffc7
Chanhook <cksgur97@gmail.com>
2022-12-03 21:07:55 +0900

    Feat: "추가 DetailActivity 구현"
    모집글에 참여할 수 있는 액티비티 추가

--- app/src/main/java/com/example/atm/DetailActivity.kt
+++ app/src/main/java/com/example/atm/DetailActivity.kt
@@ -1,11 +1,60 @@
 package com.example.atm
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.atm.databinding.ActivityDetailBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    // FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+
+    // Firebase Realtime DB
+    private lateinit var databaseRef: DatabaseReference
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val joinData = intent.getSerializableExtra("join") as Join
+        val postData = intent.getSerializableExtra("post") as Post
+
+
+        binding.detailOrigin.text = postData.search.originName
+        binding.detailDestination.text = postData.search.destinationName
+
+        val currentMember = postData.currentNumberPeople
+        val requestMember = postData.requestNumberPeople
+
+        auth = FirebaseAuth.getInstance()
+        databaseRef = FirebaseDatabase.getInstance().getReference("Around-Taxi-Member")
+
+        binding.detailCurrentMember.text =
+            currentMember.toString() + " / " + requestMember.toString()
+
+        binding.detailComment.text = postData.comment
+
+        binding.joinButton.setOnClickListener {
+            val currentUser = auth.currentUser
+            if (currentUser != null) {
+                databaseRef.child("UserAccount").child(currentUser.uid).child("chatRoom")
+                    .setValue("itm1")
+                Toast.makeText(baseContext, "참여하였습니다.", Toast.LENGTH_LONG).show()
+                finish()
+            }
+
+        }
+        binding.detailCloseBtn.setOnClickListener {
+            finish()
+        }
+
+
     }
 }
