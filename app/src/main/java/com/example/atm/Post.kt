025330c56a50984dc09032cfc7ef3d7ce607dd05
Chanhook <cksgur97@gmail.com>
2022-12-05 01:42:37 +0900

    Feat: "추가 nickname 필드"

--- app/src/main/java/com/example/atm/Post.kt
+++ app/src/main/java/com/example/atm/Post.kt
@@ -6,5 +6,6 @@ data class Post(
     var search: Search = Search(null, null, null, null, null, null, null, null, null, null),
     var currentNumberPeople: Int = 0,
     var requestNumberPeople: Int = 0,
-    var comment: String = ""
+    var comment: String = "",
+    var nickname: String = ""
 ) : Serializable
