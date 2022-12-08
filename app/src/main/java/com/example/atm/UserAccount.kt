3da9a3d509620b2e7cf6cd986260524072e5700b
Chanhook <cksgur97@gmail.com>
2022-12-08 16:07:21 +0900

    Feat: "추가 UserAccount constructor"

--- app/src/main/java/com/example/atm/UserAccount.kt
+++ app/src/main/java/com/example/atm/UserAccount.kt
@@ -3,10 +3,10 @@ package com.example.atm
 import java.io.Serializable
 
 data class UserAccount(
-    val emailID: String,
-    val password: String,
-    val gender: String,
-    val nickName: String,
+    val emailID: String="",
+    val password: String="",
+    val gender: String="",
+    val nickName: String="",
     val like: Int = 0,
     val chatRoom: String = "None"
 ) : Serializable
